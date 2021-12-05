3eb29c6792e0598a31439f85c742011d1524680f
Steven Broad <steven.broad@energyexemplar.com>
2021-12-04 20:21:31 -0800

    restructure of main and edit

--- example2.py
+++ example2.py
@@ -1,11 +1,16 @@
 from tkinter import *
 
-top = Tk()
+def onclick():
+   pass
 
-def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
+root = Tk()
+text = Text(root)
+text.insert(INSERT, "Hello.....")
+text.insert(END, "Bye Bye.....")
+text.pack()
 
-B = Button(top, text ="Hello", command = helloCallBack)
-
-B.pack()
-top.mainloop()
+text.tag_add("here", "1.0", "1.4")
+text.tag_add("start", "1.8", "1.13")
+text.tag_config("here", background="yellow", foreground="blue")
+text.tag_config("start", background="black", foreground="green")
+root.mainloop()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os, json
 import tkinter as tk
-from tkinter import filedialog as fd, ttk, Tk
+from tkinter import filedialog as fd, ttk, Tk, scrolledtext as st
+from tkinter.constants import END
 
 class FilePicker(tk.Frame):
     def __init__(self, master, label, filetypes):
@@ -12,14 +14,34 @@ class FilePicker(tk.Frame):
         self.entry = ttk.Entry(self, text='', width=100)
         self.entry.grid(column=1,row=0)
         self.entry['textvariable'] = self.contents
-        self.button = ttk.Button(self, text='...', command=self.callback, width=2)
-        self.button.grid(column=2,row=0)
+        self.choose_file_btn = ttk.Button(self, text='...', command=self.select_file, width=2)
+        self.choose_file_btn.grid(column=2,row=0)
+        self.create_file_btn = ttk.Button(self, text='+', command=self.create_file, width=2)
+        self.create_file_btn.grid(column=3,row=0)
 
-    def callback(self):
+    def select_file(self):
         self.contents.set(fd.askopenfilename(filetypes=self.filetypes))
+    
+    def create_file(self):
+        self.contents.set(fd.asksaveasfilename(filetypes=self.filetypes))
+        with open(self.get_file_path(), 'w') as fp:
+            json.dump({}, fp)
     
     def get_file_path(self):
         return self.contents.get()
+
+class ButtonRow(tk.Frame):
+    def __init__(self, master, *buttons):
+        super().__init__(master=master)
+        self.buttons = [ttk.Button(self, text=label, command=action) for label, action in buttons]
+        for idx, btn in enumerate(self.buttons):
+            btn.grid(column=idx, row=0)
+
+class RTEditWindow(Tk):
+    def __init__(self, config_file, log_message):
+        super().__init__(screenName='RTEditWindow', baseName='RTEditWindow', className='RTEditWindow')
+        self.title('Edit/Verify ' + config_file)
+        log_message('**Editing {}', config_file)
 
 class RTMainWindow(Tk):
     def __init__(self):
@@ -27,23 +49,30 @@ class RTMainWindow(Tk):
         self.title('PLEXOS Techno-economic Roundtrip Study')
         frm = ttk.Frame(self, padding=10)
         frm.grid()
-        self.exec_log = tk.StringVar()
         self.config_json = FilePicker(frm, 'Round Trip Config', [('JSON File','*.json')])
         self.config_json.grid(row=0)
-        self.config_json = FilePicker(frm, 'My Test', [('PLEXOS .xml','*.xml')])
-        self.config_json.grid(row=2)
 
-        btnfrm = ttk.Frame(frm)
-        btnfrm.grid(row=1)
-        self.launch = ttk.Button(btnfrm, text='Execute')
-        self.launch.grid(column=0,row=1)
-        self.edit = ttk.Button(btnfrm, text='Edit')
-        self.edit.grid(column=1,row=1)
-        self.cancel = ttk.Button(btnfrm, text='Cancel')
-        self.cancel.grid(column=2,row=1)
-        # self.log = ttk.Text(frm)
-        # self.log['textvariable'] = self.exec_log
-        # self.exec_log.set('Test')
+        ButtonRow(frm, ('Execute',self.execute_roundtrip), ('Edit', self.edit_rtconfig), ('Cancel', self.destroy)).grid(row=1)
+
+        self.log = st.ScrolledText(frm)
+        self.log.grid(row=3)
+        self.log.insert(END, '*'*25)
+
+        ButtonRow(frm, ('Open PLEXOS', None), ('Open PSS/E', None), ('Open Logfile', None)).grid(row=4)
+    
+    def log_message(self, fmt, *args, **kargs):
+        self.log.insert(END, '\n' + fmt.format(*args, **kargs))
+
+    def execute_roundtrip(self):
+        if not os.path.exists(self.config_json.get_file_path()):
+            self.edit_rtconfig()
+        else:
+            self.log_message('*****{}*****', self.config_json.get_file_path())
+            self.config_jobj = json.load(open(self.config_json.get_file_path()))
+            self.log_message('{}', json.dumps(self.config_jobj)[:1000])
+
+    def edit_rtconfig(self):
+        RTEditWindow(self.config_json.get_file_path(), self.log_message).mainloop()
 
 
 
